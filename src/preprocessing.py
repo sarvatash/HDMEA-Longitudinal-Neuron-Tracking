555405d93191b69d30308fe3ec191ed927a34949
Sarv Atash <Sarvenaz.atash@gmail.com>
2026-07-22 19:49:37 +0200

    Create preprocessing.py

---
+++ src/preprocessing.py
@@ -0,0 +1,15 @@
+"""
+HD-MEA preprocessing module.
+
+This module loads raw Maxwell Biosystems HDF5 recordings,
+extracts recording metadata,
+reconstructs electrode geometry,
+and prepares electrophysiological data
+for downstream spike sorting and analysis.
+
+Author:
+Sarv Atashian
+
+Developed during the M.Sc. thesis,
+University of Naples Federico II.
+"""
