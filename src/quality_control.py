ac982c7b89a8b363403abe2d951c2c39e2a11e80
Sarv Atash <Sarvenaz.atash@gmail.com>
2026-07-22 19:47:18 +0200

    Create quality_control.py

---
+++ src/quality_control.py
@@ -0,0 +1,9 @@
+"""
+Quality-control module.
+
+Implements filtering criteria based on waveform amplitude,
+ISI violations, spatial consistency, and one-to-one neuron matching.
+
+Author:
+Sarv Atashian
+"""
