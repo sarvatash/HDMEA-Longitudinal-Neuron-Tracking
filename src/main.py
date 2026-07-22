3e688d2f6d907e7845adc6b0b30f1ccd286fb27c
Sarv Atash <Sarvenaz.atash@gmail.com>
2026-07-22 19:45:14 +0200

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+"""
+HD-MEA Longitudinal Neuron Tracking
+
+Master's Thesis
+University of Naples Federico II
+
+Author: Sarv Atashian
+"""
+
+def main():
+    print("HD-MEA Longitudinal Neuron Tracking Pipeline")
+
+if __name__ == "__main__":
+    main()
