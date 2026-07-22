9ca2ac62cb219bd53a514bca7c676315462efa83
Sarv Atash <Sarvenaz.atash@gmail.com>
2026-07-22 19:47:54 +0200

    Create visualization.py

---
+++ src/visualization.py
@@ -0,0 +1,10 @@
+"""
+Visualization module.
+
+Functions for generating publication-quality figures
+including spatial maps, waveform plots,
+and firing-rate analysis.
+
+Author:
+Sarv Atashian
+"""
